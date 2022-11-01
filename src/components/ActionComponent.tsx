267cad722ecdcefdb3eea61856fbcb29c725a82d
jelenamusic10 <jelena.music@consulteer.com>
2022-11-01 11:48:48 +0100

    landing page

--- src/components/ActionComponent.tsx
+++ src/components/ActionComponent.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import logo from '../assets/logo.svg';
 import { connect } from 'react-redux';
 import '../App.css';
 
 const ActionComponent = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Consulteer React Boilerplate</p>
+      </header>
+    </div>
+  );
 };
 
 export default connect(null)(ActionComponent);
